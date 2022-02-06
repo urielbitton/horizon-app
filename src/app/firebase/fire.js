e340001b62ad8f340e1415a858f9212dab1b707d
Atomics <info@atomicsdigital.com>
2022-02-06 14:38:10 -0500

    firebase connect

--- src/app/firebase/fire.js
+++ src/app/firebase/fire.js
@@ -1,12 +1,12 @@
 import firebase from "firebase"
  
 const config = {
-  apiKey: "AIzaSyDInFmYC9-FBGE6MG7496SeaE-DAsW8I_g",
-  authDomain: "solaris-app-22479.firebaseapp.com",
-  projectId: "solaris-app-22479",
-  storageBucket: "solaris-app-22479.appspot.com",
-  messagingSenderId: "457307118139",
-  appId: "1:457307118139:web:77010509fdb2c5d2aada1e"
+  apiKey: "AIzaSyBQjj3VWnTzHDUoQPFGbV6ufJbxw8qkgLU",
+  authDomain: "horizon-app-fbc9d.firebaseapp.com",
+  projectId: "horizon-app-fbc9d",
+  storageBucket: "horizon-app-fbc9d.appspot.com",
+  messagingSenderId: "578689248315",
+  appId: "1:578689248315:web:b0db3d2496b2d38e81801e"
 }
 const firebaseApp = firebase.initializeApp(config)
 
