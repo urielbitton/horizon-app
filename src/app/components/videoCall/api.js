0c6daf484bc8ac7fcddb08bcf03818da2cd4abd9
Atomics <info@atomicsdigital.com>
2022-02-21 00:36:22 -0500

    updated api region

--- src/app/components/videoCall/api.js
+++ src/app/components/videoCall/api.js
@@ -23,7 +23,6 @@ export const createMeeting = async ({ token }) => {
   const options = {
     method: "POST",
     headers: { Authorization: token, "Content-Type": "application/json" },
-    body: JSON.stringify({ region: "us001" })
   };
 
   const { meetingId } = await fetch(url, options)
