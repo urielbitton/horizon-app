9e7263d56f1773801d3f55fbd8d17a5c158cc9ea
Atomics <info@atomicsdigital.com>
2022-02-21 00:35:08 -0500

    toggle recording meeting

--- src/app/components/videoCall/VideoCallScreen.js
+++ src/app/components/videoCall/VideoCallScreen.js
@@ -10,13 +10,13 @@ export default function VideoCallScreen(props) {
   const { ParticipantsView, ConnectionsView, participantViewVisible, leave,
     toggleWebcam, toggleMic, localWebcamOn, localMicOn, meetingID,
     toggleScreenShare, localParticipant } = props
-
   const onStreamEnabled = (stream) => {}
   const onStreamDisabled = (stream) => {}
   const [showTools, setShowTools] = useState(true)
-  const { participants } = useMeeting()
-  const { screenShareOn, screenShareStream, webcamStream } = useParticipant(localParticipant?.id, { onStreamEnabled, onStreamDisabled })
+  const { participants, isRecording, startRecording, stopRecording } = useMeeting()
+  const { screenShareOn, screenShareStream } = useParticipant(localParticipant?.id, { onStreamEnabled, onStreamDisabled })
   const participantsNum = participants.size
+  console.log(isRecording)
 
   const copyMeetingID = () => {
     navigator.clipboard.writeText(meetingID)
@@ -66,11 +66,13 @@ export default function VideoCallScreen(props) {
           <div 
             className={!localMicOn ? 'inactive' : 'active'}
             onClick={toggleMic}
+            title={localMicOn ? 'Mute Mic' : 'Unmute Mic'}
           >
             <i className={`fal ${localMicOn ? "fa-microphone" : "fa-microphone-slash"}`}></i>
           </div>
           <div 
             className={!localWebcamOn ? 'inactive' : 'active'}
+            title={localWebcamOn ? 'Disable Webcam' : 'Enable Webcam'}
             onClick={() => {
               toggleWebcam()
               screenShareOn && toggleScreenShare()
@@ -81,24 +83,30 @@ export default function VideoCallScreen(props) {
           <div 
             className={screenShareOn ? 'active' : 'inactive'}
             onClick={toggleScreenShare}
+            title={screenShareOn ? 'Stop Screen Share' : 'Start Screen Share'}
           >
             <i className="fal fa-desktop"></i>
             { !screenShareOn && <i className="fal fa-slash"></i>}
           </div>
-          <div>
+          <div title="Open Chats">
             <i className="fal fa-comment-alt"></i>
           </div>
-          <div>
+          <div className="View Participants">
             <i className="fal fa-user-friends"></i>
           </div>
-          <div onClick={() => copyMeetingID()}>
-            <i className="fal fa-info-circle"></i>
+          <div 
+            className={isRecording ? 'active' : 'inactive'}
+            onClick={()  => !isRecording ? startRecording() : stopRecording()}            
+            title="Record Meeting"
+          >
+            <i className="fal fa-record-vinyl"></i>
           </div>
-          <div>
+          <div title="More Options">
             <i className="fal fa-ellipsis-h"></i>
           </div>
           <div 
             className="red"
+            title="End Meeting"
             onClick={() => endMeeting()}
           >
             <i className="fal fa-phone"></i>
